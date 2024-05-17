2a407b7b1507f64da9bd0035dcc5724db9fe98d1
Mark Eschbach <meschbach@gmail.com>
2024-05-17 11:14:29 -0700

    cd/github: prs will now wait for the secret to exist before waiting on population -- kubectl wait will bail if the resource does not exist

--- e2e/run-tests.sh
+++ e2e/run-tests.sh
@@ -2,6 +2,15 @@
 
 set -xe
 kubectl apply -f e2e-pg16-gen-db-same-namespace.yaml
+
+echo
+echo "Waiting for target secret to be created."
+echo
+until kubectl get secrets --namespace e2e-pg16 |grep database-sample; do sleep 5; done
+
+echo
+echo "Waiting new secret to be populated, then verify"
+echo
 kubectl wait --namespace e2e-pg16 --for='jsonpath={.data.host}="cGcxNi5lMmUtcGcxNi5zdmMuY2x1c3Rlci5sb2NhbA=="' secret/database-sample
 # ensure it is a new database
 db_name=$(kubectl get secret --namespace e2e-pg16 -o json database-sample |jq -r '.data.database' |base64 -d)
