529def04fea6e14c3e8115bb0700ae866beffa70
Mark Eschbach <meschbach@gmail.com>
2024-05-18 09:22:05 -0700

    testing/e2e: wait on ready field instead of the secret being created

--- e2e/run-tests.sh
+++ e2e/run-tests.sh
@@ -9,16 +9,15 @@ kubectl apply -f e2e-pg16-gen-db-same-namespace.yaml
 echo
 echo "Waiting for target secret to be created."
 echo
-until kubectl get secrets --namespace e2e-pg16 |grep database-sample; do sleep 5; done
-
+kubectl wait --namespace e2e-pg16 --for='jsonpath={.status.ready}=true' pgdb.storage.meschbach.com/database
 echo
 echo "Waiting new secret to be populated, then verify"
 echo
 kubectl wait --namespace e2e-pg16 --for='jsonpath={.data.host}="cGcxNi5lMmUtcGcxNi5zdmMuY2x1c3Rlci5sb2NhbA=="' secret/database-sample
-# ensure it is a new database
+# ensure the target host makes sense
 db_name=$(kubectl get secret --namespace e2e-pg16 -o json database-sample |jq -r '.data.database' |base64 -d)
 if [ "$db_name" = "postgres" ]; then
-    echo "New database was not created."
+    echo "Target database may not be postgres."
 fi
 
 # TODO: need to trap signals to cleanup locally in bad test cases.
